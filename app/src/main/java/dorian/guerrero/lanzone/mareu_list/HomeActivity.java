e613d8ee5695247a06a628f3ab970e2bb88d9d1b
Praal0 <guerrero.dorian1@gmail.com>
2021-02-16 10:56:33 +0100

    Add ignore

--- app/src/main/java/dorian/guerrero/lanzone/mareu_list/HomeActivity.java
+++ app/src/main/java/dorian/guerrero/lanzone/mareu_list/HomeActivity.java
@@ -1,22 +1,36 @@
 package dorian.guerrero.lanzone.mareu_list;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import dorian.guerrero.lanzone.R;
 
 public class HomeActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        final FloatingActionButton button = findViewById(R.id.addMeeting);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent addIntent = new Intent(HomeActivity.this, AddReuActivity.class);
+                startActivity(addIntent);
+            }
+        });
+
     }
 
 
+
+
 }
+
+
