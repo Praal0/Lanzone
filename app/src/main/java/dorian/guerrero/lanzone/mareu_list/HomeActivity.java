5aa1d1c3cb9f036cb7e641fd144b513c5c572fc0
Praal0 <guerrero.dorian1@gmail.com>
2021-02-18 13:49:46 +0100

    P4_Dorian_Guerrero_Add_Meeting

--- app/src/main/java/dorian/guerrero/lanzone/mareu_list/HomeActivity.java
+++ app/src/main/java/dorian/guerrero/lanzone/mareu_list/HomeActivity.java
@@ -1,6 +1,8 @@
 package dorian.guerrero.lanzone.mareu_list;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,20 +13,32 @@ import dorian.guerrero.lanzone.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private FloatingActionButton addButton;
+    private RecyclerView mRecyclerView;
+    private String s1[],s2[];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        final FloatingActionButton button = findViewById(R.id.addMeeting);
-        button.setOnClickListener(new View.OnClickListener() {
+        mRecyclerView = findViewById(R.id.rcvMeeting);
+
+        s1 = getResources().getStringArray(R.array.progamming_language);
+        s2 = getResources().getStringArray(R.array.email);
+
+        addButton = findViewById(R.id.addMeeting);
+        addButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
                 Intent addIntent = new Intent(HomeActivity.this, AddReuActivity.class);
                 startActivity(addIntent);
             }
         });
+
+        MeetingAdapter myAdapter = new MeetingAdapter(this,s1,s2);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
     }
 
