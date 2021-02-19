703dcdf920200713f8d74ad1df746a44d5cb1b8a
Praal0 <guerrero.dorian1@gmail.com>
2021-02-19 12:06:29 +0100

    P4_Dorian_GUerrero
    Add : New Class Room and picture for show in list of meeting

--- app/src/main/java/dorian/guerrero/lanzone/events/AddMeetingEvent.java
+++ app/src/main/java/dorian/guerrero/lanzone/events/AddMeetingEvent.java
@@ -1,4 +1,18 @@
 package dorian.guerrero.lanzone.events;
 
+import dorian.guerrero.lanzone.model.Meeting;
+
+/**
+ * Event fired when a user add a Meeting
+ */
+
 public class AddMeetingEvent {
+    public Meeting meeting;
+
+    /**
+     * Constructor
+     * @param meeting
+     */
+
+    public AddMeetingEvent(Meeting meeting){this.meeting = meeting;};
 }
--- app/src/main/java/dorian/guerrero/lanzone/service/DummyMeetingApiService.java
+++ app/src/main/java/dorian/guerrero/lanzone/service/DummyMeetingApiService.java
@@ -1,4 +1,38 @@
 package dorian.guerrero.lanzone.service;
 
-public class DummyMeetingApiService {
+import java.util.ArrayList;
+import java.util.List;
+
+import dorian.guerrero.lanzone.model.Meeting;
+
+public class DummyMeetingApiService implements MeetingApiService{
+    private List<Meeting> lstMeeting = new ArrayList<>();
+
+    /**
+     * {@inheritDoc}
+     */
+
+    public List<Meeting> getNeighbours() {
+        return lstMeeting;
+    }
+
+    /**
+     * Create a new Meeting in list
+     * @param meeting
+     */
+    public void createNeighbour(Meeting meeting) {
+        lstMeeting.add(meeting);
+    }
+
+    /**
+     * Remove Meeting in list
+     */
+    public void deleteNeighbour(Meeting meeting) {
+        lstMeeting.remove(meeting);
+
+    }
+
+
+
+
 }
